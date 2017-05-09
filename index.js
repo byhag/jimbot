1b2e57d42a59f364569f60a4f21fd07d0502fa8c
byhag <byhag4007@gmail.com>
2017-05-09 05:07:10 -0500

    Add image; adding cool guy bot code

--- index.js
+++ index.js
@@ -1,25 +1,87 @@
+var FeedParser = require('feedparser');
+var request = require('request');
 var cool = require('cool-ascii-faces');
-var express = require('express');
-var app = express();
+var director = require('director');
+var http = require('http');
+var https = require('https');
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+router = new director.http.Router({
+  '/': {
+    post: respond,
+    get: ping
+  }
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
+server = http.createServer(function (req, res) {
+  req.chunks = [];
+  req.on('data', function(chunk) {
+    req.chunks.push(chunk.toString());
+  });
+
+  router.dispatch(req, res, function(err) {
+    res.writeHead(err.status, {"Content-Type": "text/plain"});
+    res.end(err.message);
+  });
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+port = Number(process.env.PORT || 5000);
+server.listen(port);
 
+var botID = process.env.BOT_ID
+
+function respond() {
+  var request = JSON.parse(this.req.chunks[0]),
+      botRegex = /^\/test bot$/;
+
+  if(request.text && botRegex.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage();
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+}
+
+function postMessage() {
+  var botResponse, options, body, botReq;
+
+  botResponse = cool();
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+
+function ping() {
+  this.res.writeHead(200);
+  this.res.end("Hi there");
+}
 
