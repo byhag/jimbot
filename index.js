8ac01de0605833170f9912ef44c45ab83b40d821
byhag <byhag4007@gmail.com>
2017-05-09 05:18:41 -0500

    Update regex on test bot

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ var botID = process.env.BOT_ID
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/test bot$/;
+      botRegex = /test bot/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
