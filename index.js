67502f3f8b40262e30538742ad023b495739948f
byhag <byhag4007@gmail.com>
2017-05-10 13:35:52 -0500

    Add hello(); begin adding quote functionality

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ var cool = require('cool-ascii-faces');
 var director = require('director');
 var http = require('http');
 var https = require('https');
+var fs = require('fs');
 
 router = new director.http.Router({
   '/': {
     post: respond,
     get: ping 
+  },
+  '/~d/styles/rss2full.xsl': {
+    get: rss
   }
 });
 
@@ -24,6 +28,10 @@ server = http.createServer(function (req, res) {
     res.end(err.message);
   });
 });
+
+function rss() {
+
+}
 
 port = Number(process.env.PORT || 5000);
 server.listen(port);
@@ -43,7 +51,7 @@ function respond() {
     this.res.end();
   } else if (request.text && hiRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage('Hi there');
+    hello();
     this.res.end();
   } else if (request.text && faceRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -54,6 +62,26 @@ function respond() {
     this.res.writeHead(200);
     this.res.end();
   }
+}
+
+function hello() {
+  var choice = Math.floor(Math.random() * 4);
+  var response;
+  switch (choice) {
+    case 0:
+      response = 'Hi there';
+      break;
+    case 1:
+      response = 'Hi';
+      break;
+    case 2:
+      response = 'Hello';
+      break;
+    case 3:
+      response = 'Hey';
+      break;
+  }
+  postMessage(response);
 }
 
 function joke() {
@@ -91,6 +119,43 @@ function joke() {
   jokeReq.end();
 }
 
+function quote() {
+  var options = {
+    hostname: 'feeds.feedburner.com',
+    path: '/brainyquote/QUOTEBR',
+    method: 'GET'
+  }
+
+  var serv = this;
+
+  var quoteReq = https.request(options, function(res) {
+    if(res.statusCode == 200) {
+      //good
+    } else {
+      console.log('bad status code ' + res.statusCode);
+    }
+    var str = '';
+    res.on('data', function(d) {
+      str += d;
+    });
+
+    res.on('end', function() {
+      // var obj = JSON.parse(str);
+      serv.res.writeHead(200);
+      serv.res.end(str);
+      // postMessage(obj.joke);
+    });
+  });
+
+  quoteReq.on('error', function(err) {
+    console.log('error getting quote ' + JSON.stringify(err));
+  });
+  quoteReq.on('timeout', function(err) {
+    console.log('timeout getting quote ' + JSON.stringify(err));
+  });
+  quoteReq.end();
+}
+
 function postMessage(botResponse) {
   var botResponse, options, body, botReq;
 
@@ -124,9 +189,9 @@ function postMessage(botResponse) {
   botReq.end(JSON.stringify(body));
 }
 
-
 function ping() {
+  var random = Math.floor(Math.random() * 3);
   this.res.writeHead(200);
-  this.res.end("Hi there");
+  this.res.end("random number: " + random);
 }
 
